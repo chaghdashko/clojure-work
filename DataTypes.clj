cb45191fb22e0c0f69d2e1155140f6534289fe5e
Cagdas <dc7780@gmail.com>
2017-12-31 22:25:12 +0300

    Create DataTypes.clj

---
+++ DataTypes.clj
@@ -0,0 +1,15 @@
+(defn example []
+  ;; declares an integer variable
+  (def x 1)
+
+  ;; declares a float variable
+  (def y 1.25)
+
+  ;; declares a string variable
+  (def str1 "Hello")
+
+  (println x)
+  (println y)
+  (println str1))
+
+(example)
